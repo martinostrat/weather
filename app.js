30274aac371ad3269613d18723bf236c5d6465f9
Martin <martin.ostrat@khk.ee>
2022-01-14 10:18:23 +0200

    Display city weather data from API

---
+++ app.js
@@ -0,0 +1,29 @@
+function weatherDataFetch(city) {
+    let key = 'b05a100c6e6c27146f81ad75c338bae4';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(resp => {
+        return resp.json();
+    })
+    .then(data => {
+        console.log(data);
+        drawWeather(data);
+    })
+    .catch(() => {
+        console.log('error');
+    })
+}
+
+function cityWeather(e) {
+    weatherDataFetch('Tallinn');
+}
+
+function drawWeather(data) {
+    let cel = Math.round(parseFloat(data.main.temp) - 273.15);
+    let desc = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = desc;
+    document.querySelector('#temp').innerHTML = cel + '&deg;';
+    document.querySelector('#location').innerHTML = data.name;
+}
+
+cityWeather();
